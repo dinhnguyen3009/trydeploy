e621d56549203daa34b299d14965fe9f617aa510
dinhnguyen3009 <nguyentdinh3009@gmail.com>
2019-04-24 19:47:03 +0700

    Updates

---
+++ precache-manifest.6defd17cec23fb468df34afe58a7829e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77b012d5b0b3847631b6788c30d292f",
+    "url": "/trydeploy/index.html"
+  },
+  {
+    "revision": "b1d710113f6b4782356b",
+    "url": "/trydeploy/static/css/main.37b697f4.chunk.css"
+  },
+  {
+    "revision": "57822fb59c2550c00542",
+    "url": "/trydeploy/static/js/2.1664e62a.chunk.js"
+  },
+  {
+    "revision": "b1d710113f6b4782356b",
+    "url": "/trydeploy/static/js/main.88cd7c7f.chunk.js"
+  },
+  {
+    "revision": "75f2cb560d0c5683155b",
+    "url": "/trydeploy/static/js/runtime~main.db8f1fb4.js"
+  }
+]);
